cb40f678ccb44e64fdf5b646c8965b8deadd831c
nianer-huaixu <1836829002@qq.com>
2025-05-19 16:00:29 +0800

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import "@/styles/globals.css";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <Header/>
+    <Component {...pageProps} />
+    <Footer/>
+  </>
 }
